644335efbce5af478fc58bc3779b79b3eba1607d
Leonardo <desu987@hotmail.com>
2022-03-18 23:13:02 -0300

    Added(dependency): added pg and typeorm dependencies

--- client/src/shared/infra/http/routes/index.ts
+++ client/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { createConnection } from 'typeorm';
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
 
+createConnection();
 const routes = Router();
 
 routes.use('/vehicles', usersRouter);
