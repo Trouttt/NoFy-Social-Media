52cfc193058c1f2c4e991101ea55252523483e6f
Leonardo <desu987@hotmail.com>
2022-03-18 23:31:47 -0300

    Added(controller): added user controller

--- client/src/modules/users/infra/http/routes/users.routes.ts
+++ client/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
+const usersController = new UsersController();
+usersRouter.get('/', usersController.create);
 
 export default usersRouter;
