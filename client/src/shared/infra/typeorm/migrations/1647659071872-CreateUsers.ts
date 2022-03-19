5855337883b64e86fc04b31e585e0de94bfd81da
Leonardo <desu987@hotmail.com>
2022-03-19 00:08:46 -0300

    Added(migration): added user migration

---
+++ client/src/shared/infra/typeorm/migrations/1647659071872-CreateUsers.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1647659071872 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
