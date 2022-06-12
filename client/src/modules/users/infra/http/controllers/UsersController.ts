68eaf29c4f2edee485a9316db8551377c0519fb5
Leonardo <desu987@hotmail.com>
2022-06-12 17:49:16 -0300

    Added(routes): added celebrate in post route

--- client/src/modules/users/infra/http/controllers/UsersController.ts
+++ client/src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express';
 
 export default class UsersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    return response.status(200);
+    return response.status(200).json({});
   }
 }
--- client/src/modules/users/infra/http/routes/users.routes.ts
+++ client/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const usersRouter = Router();
 const usersController = new UsersController();
-usersRouter.get('/', usersController.create);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      age: Joi.string().required(),
+      email: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 export default usersRouter;
